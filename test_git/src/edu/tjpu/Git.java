4d54b59c057a7c7a4800cc11e05ae58ed5b22ada
panjunwen <1591776456@qq.com>
2020-07-04 19:33:59 +0800

    将工程加入到本地仓库

---
+++ test_git/src/edu/tjpu/Git.java
@@ -0,0 +1,12 @@
+package edu.tjpu;
+
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+    }
+}

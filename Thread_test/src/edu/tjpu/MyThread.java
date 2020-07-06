278c2d5eb43f45683e5769d9982c55a24bda5235
panjunwen <1591776456@qq.com>
2020-07-06 17:39:25 +0800

    代码修改

--- Thread_test/src/edu/tjpu/MyThread.java
+++ Thread_test/src/edu/tjpu/MyThread.java
@@ -1,5 +1,9 @@
 package edu.tjpu;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
 public class MyThread extends Thread{
     @Override
     public void run() {
@@ -8,13 +12,31 @@ public class MyThread extends Thread{
         }
     }
 }
+class MyCallable implements Callable<String >{
 
+
+    @Override
+    public String call() throws Exception {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(Thread.currentThread().getName()+":"+i);
+        }
+        return "线程执行完了";
+    }
+}
 class TestMyThread{
-    public static void main(String[] args) {
-        Thread t1 = new MyThread();
+    public static void main(String[] args)  {
+        FutureTask<String> futureTask=new FutureTask<String>(new MyCallable());
+        Thread t1=new Thread(futureTask);
         t1.start();
         for (int i = 0; i < 10; i++) {
             System.out.println(Thread.currentThread().getName()+":"+i);
+        }
+        try {
+            System.out.println(futureTask.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
         }
     }
 }

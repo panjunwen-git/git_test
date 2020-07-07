668154edff7efb627d46163c3a2389d304baa3da
panjunwen <1591776456@qq.com>
2020-07-07 19:56:16 +0800

    代码修改

---
+++ Thread_test/src/edu/tjpu/Test.java
@@ -0,0 +1,10 @@
+package edu.tjpu;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Test {
+    public static void main(String[] args) {
+        Set<String> set=new HashSet<String>();
+    }
+}
